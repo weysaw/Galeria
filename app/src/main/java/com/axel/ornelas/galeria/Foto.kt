61d1e5900c604cc249fd7b7c6ef1215441a4b7d0
Axel L Ornelas M <axel.lom17@gmail.com>
2021-10-21 04:37:08 -0700

    Se remueve parcelize y la direccion se hace string

--- app/src/main/java/com/axel/ornelas/galeria/Foto.kt
+++ app/src/main/java/com/axel/ornelas/galeria/Foto.kt
@@ -1,14 +1,10 @@
 package com.axel.ornelas.galeria
 
-import android.net.Uri
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 import java.util.*
 
-@Parcelize
 data class Foto(
-    val direccionUri: Uri,
-    val descripcion: String,
+    val direccionUri: String,
+    var descripcion: String,
     var fecha: Long = Date().time
-) : Parcelable, Serializable
+) : Serializable
